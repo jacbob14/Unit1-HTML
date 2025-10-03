058eec2d799259f72fe9bf596a4d7268c3c0ea60
Jacob Bobrov <jacbob29@bergen.org>
2025-10-02 20:56:23 -0400

    cart 10/2

--- L09-Containers/index.js
+++ L09-Containers/index.js
@@ -1,5 +1,31 @@
-window.alert("success");
+const cart = document.getElementById("cart-ul");
+const cartItems = document.getElementById("cart-items");
 
-function cart() {
-    alert("Success");
+function cartAppend(itemName, itemPrice) {
+    const item = document.createElement("li");
+    item.textContent = itemName;
+    cart.appendChild(item);
+
+    let rows = cartItems.rows;
+    for (let i=1; i<rows.length; i++) {
+        console.log(rows[i])
+    }
+
+    const itemData = document.createElement("tr");
+    const itemType = document.createElement("td");
+    itemType.textContent = itemName;
+    itemData.appendChild(itemType);
+    const quantity = document.createElement("td");
+    quantity.textContent = "1";
+    itemData.appendChild(quantity);
+    const price = document.createElement("td");
+    price.textContent = itemPrice;
+    itemData.appendChild(price);
+    cartItems.appendChild(itemData);
+
+}
+
+function clearCart() {
+    cart.innerHTML = "";
+
 }
